fdb6f71eca9dfb9354f66b1d001ee5a42654ab21
pjo901018 <pjo901018@korea.ac.kr>
2018-01-22 15:51:51 +0900

    Sort

---
+++ src/com/asuscomm/yangyicompany/f_sort/Dish.java
@@ -0,0 +1,29 @@
+package com.asuscomm.yangyicompany.f_sort;
+
+public class Dish {
+    String name;
+    boolean vegeterian;
+    int calories;
+    Type type;
+
+    public Dish(String name, boolean vegeterian, int calories, Type type) {
+        this.name = name;
+        this.vegeterian = vegeterian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public enum Type {
+        MEAT, OTHER, NOODLE, FRUIT, FISH
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", vegeterian=" + vegeterian +
+                ", calories=" + calories +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ src/com/asuscomm/yangyicompany/f_sort/SortMain.java
@@ -0,0 +1,32 @@
+package com.asuscomm.yangyicompany.f_sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortMain {
+    public static void main(String[] args) {
+        List<Dish> dishes = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 450, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 300, Dish.Type.OTHER),
+                new Dish("spagetti", true, 400, Dish.Type.NOODLE),
+                new Dish("apple", true, 300, Dish.Type.FRUIT),
+                new Dish("melon", true, 320, Dish.Type.FRUIT),
+                new Dish("salmon", true, 420, Dish.Type.FISH),
+                new Dish("prawn", true, 410, Dish.Type.FISH)
+        );
+
+        List<String> collect = dishes.stream()
+                .filter(dish -> dish.calories < 400)
+                .sorted(Comparator.comparing(dish -> dish.calories))
+                .map(dish -> dish.name)
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+
+    }
+}
