9ab429c12ff0d00e6d4e3e33895eb98983145463
pjo901018 <pjo901018@korea.ac.kr>
2018-01-23 10:58:52 +0900

    Complete query challenges

--- src/com/asuscomm/yangyicompany/k_transaction/TransactionMain.java
+++ src/com/asuscomm/yangyicompany/k_transaction/TransactionMain.java
@@ -3,6 +3,7 @@ package com.asuscomm.yangyicompany.k_transaction;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class TransactionMain {
     public static void main(String[] args) {
@@ -43,25 +44,55 @@ public class TransactionMain {
 
 
         // Query 3: Find all traders from Cambridge and sort them by name.
-
+        System.out.println("");
+        System.out.println("Query 3: Find all traders from Cambridge and sort them by name.");
+        transactions.stream().map(Transaction::getTrader)
+                .filter(trader -> "Cambridge".equals(trader.getCity()))
+                .sorted(Comparator.comparing(Trader::getName))
+                .forEach(System.out::println);
 
 
         // Query 4: Return a string of all traders’ names sorted alphabetically.
+        System.out.println("");
+        System.out.println("Query 4: Return a string of all traders’ names sorted alphabetically.");
+        Optional<String> reduce = transactions.stream().map(Transaction::getTrader)
+                .map(Trader::getName)
+                .sorted(String::compareTo)
+                .reduce((String s1, String s2) -> s1 + s2);
+
+        System.out.println(reduce.get());
 
 
 
         // Query 5: Are there any trader based in Milan?
+        System.out.println("");
+        System.out.println("Query 5: Are there any trader based in Milan?");
+        boolean b = transactions.stream().map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .anyMatch("Millan"::equals);
 
+        System.out.println(b);
 
 
 
         // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
-
+        System.out.println("");
+        System.out.println("Query 6: Update all transactions so that the traders from Milan are set to Cambridge.");
+        System.out.println("Before query");
+        transactions.stream().forEach(System.out::println);
+        transactions.stream().map(Transaction::getTrader)
+                .filter(trader -> "Cambridge".equals(trader.getCity()))
+                .forEach(trader -> trader.setCity("Milan"));
+        System.out.println("After query");
+        transactions.stream().forEach(System.out::println);
 
 
 
         // Query 7: What's the highest value in all the transactions?
-
-
+        System.out.println("");
+        System.out.println("Query 7: What's the highest value in all the transactions?");
+        int asInt = transactions.stream().mapToInt(Transaction::getValue)
+                .max().getAsInt();
+        System.out.println("Answer : " + asInt);
     }
 }
