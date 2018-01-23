53f230876bfb05d94ab863df576f147d8ffa8294
pjo901018 <pjo901018@korea.ac.kr>
2018-01-23 10:32:46 +0900

    forEachOrdered sorted filter

---
+++ src/com/asuscomm/yangyicompany/k_transaction/Transaction.java
@@ -0,0 +1,48 @@
+package com.asuscomm.yangyicompany.k_transaction;
+
+import java.util.Objects;
+
+public class Transaction{
+
+    private Trader trader;
+    private int year;
+    private int value;
+
+    public Transaction(Trader trader, int year, int value)
+    {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader(){
+        return this.trader;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public String toString(){
+        return "{" + this.trader + ", " +
+                "year: "+this.year+", " +
+                "value:" + this.value +"}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
